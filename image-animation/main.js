346643abc8a2773fd07d81bfebfd37aa7a2555e7
AdamHasma <muslim.hasmagomadov@gmail.com>
2021-01-30 18:44:12 +0100

    finally

--- image-animation/main.js
+++ image-animation/main.js
@@ -2,21 +2,21 @@ const aTag = document.querySelector(".button-a");
 const aboutLi = document.querySelector(".to-about")
 
 aTag.addEventListener("click", () => {
-  aTag.classList.add("anim");
-  document.querySelector(".unset").style.width = 'width: 100vw';
+  // aTag.classList.add("anim");
+  // document.querySelector(".unset").style.width = 'width: 100vw';
   document.querySelector(".index-title").style.display = 'none';
-  document.querySelector(".unset>img").classList.add("index-img");
+  document.querySelector(".set-class").classList.add("index-img");
   setTimeout(function() {
     window.location.assign("about.html");
-  }, 400)
+  }, 1200)
 });
 
 aboutLi.addEventListener("click", () => {
-  aboutLi.classList.add("anim");
-  document.querySelector(".unset").style.width = 'width: 100vw';
+  // aboutLi.classList.add("anim");
+  // document.querySelector(".unset").style.width = 'width: 100vw';
   document.querySelector(".index-title").style.display = 'none';
-  document.querySelector(".unset>img").classList.add("index-img");
+  document.querySelector(".set-class").classList.add("index-img");
   setTimeout(function() {
     window.location.assign("about.html");
-  }, 400)
+  }, 1200)
 });
